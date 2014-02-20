4f41c20a1c021df09067ea4c3aa1d9cf0a7a54bf
shaffer-wv <shaffer-wv@outlook.com>
2014-02-19 21:18:11 -0500

    Added database

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from app import db
+
+ROLE_USER = 0
+ROLE_ADMIN = 1
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	nickname = db.Column(db.String(64), index = True, unique = True)
+	email = db.Column(db.String(120), index = True, unique = True)
+	role = db.Column(db.SmallInteger, default = ROLE_USER)
+	tasks = db.relationship('Task', backref = 'author', lazy = 'dynamic')
+
+	def __repr__(self):
+		return '<User %r>' % (self.nickname)
+
+class Task(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	title = db.Column(db.String(120))
+	created = db.Column(db.DateTime)
+	modified = db.Column(db.DateTime)
+	completed = db.Column(db.DateTime)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+	def __repr__(self):
+		return '<Task %r>' % (self.title)
