b94532b22605685f55a56c20d64a6285051604b4
shaffer-wv <shaffer-wv@outlook.com>
2014-02-28 22:21:14 -0500

    Added Unit Testing

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from hashlib import md5
 
 ROLE_USER = 0
 ROLE_ADMIN = 1
@@ -10,8 +11,36 @@ class User(db.Model):
 	role = db.Column(db.SmallInteger, default = ROLE_USER)
 	tasks = db.relationship('Task', backref = 'author', lazy = 'dynamic')
 
+	def is_authenticated(self):
+		return True
+
+	def is_active(self):
+		return True
+
+	def is_anonymous(self):
+		return False
+
+	def get_id(self):
+		return unicode(self.id)
+
+	def avatar(self, size):
+		return 'http://www.gravatar.com/avatar/' + md5(self.email).hexdigest() + '?d=mm&s=' + str(size)
+
+	@staticmethod
+	def make_unique_nickname(nickname):
+		if User.query.filter_by(nickname = nickname).first() == None:
+			return nickname
+		version = 2
+		while True:
+			new_nickname = nickname + str(version)
+			if User.query.filter_by(nickname = new_nickname).first() == None:
+				break
+			version += 1
+		return new_nickname
+
 	def __repr__(self):
 		return '<User %r>' % (self.nickname)
+
 
 class Task(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
